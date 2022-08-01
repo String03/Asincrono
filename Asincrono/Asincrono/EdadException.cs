7bc448ef2c5490b6b7ed34aa97526f4eb59f278f
String03 <40239185+String03@users.noreply.github.com>
2022-08-01 13:43:15 -0300

    Add files via upload

---
+++ Asincrono/Asincrono/EdadException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asincrono
+{
+    public class EdadException: Exception
+    {
+        public int Edad { get; private set; }
+
+        public EdadException(int _edad):base("El registro de edad " + _edad + " está fuera de rango")
+        {
+            Edad = _edad;
+        }
+    }
+}
+    
---
+++ Asincrono/Asincrono/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asincrono
+{
+    class Program
+    {
+        public static async void MetodoAsincrono()
+        {
+            Task<string> tarea = RetrasoAsincrono();
+
+            try
+            {
+                var resultado = await tarea;
+                Console.WriteLine("Resultado {0}", resultado);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Mensaje de excepción: {0}",ex.Message);
+            }
+            Console.WriteLine("La tarea fue cancelada: {0}", tarea.IsCanceled.ToString());
+            Console.WriteLine("La tarea falló: {0}", tarea.IsFaulted.ToString());
+        
+            if(tarea.Exception != null)
+            {
+                Console.WriteLine("Manejo de Excepción {0}",tarea.Exception.Message);
+                Console.WriteLine("Manejo de Excepción Anidada {0}", tarea.Exception.InnerException.Message);
+
+            }
+
+        }
+
+        public static async Task<string> RetrasoAsincrono()
+        {
+            await Task.Delay(100);
+            //throw new OperationCanceledException("Cancelado...");
+
+            throw new Exception("Ocurrió un Problema...");
+
+            //return "Tarea Completada";
+        }
+        static void Main(string[] args)
+        {
+            //Task tarea = new Task(MetodoAsincrono);
+            //tarea.Start();
+
+            //tarea.Wait();
+
+            try
+            {
+                int edad = 140;
+
+                if (edad > 100)
+                {
+                    throw new EdadException(edad);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine(5 + 5);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
